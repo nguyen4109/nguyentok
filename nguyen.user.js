181526a1c3763a0648272ecdad54ab4e0d3c7359
nguyen4109 <jayatamamaju@gmail.com>
2025-03-23 22:43:31 +0700

    Update nguyen.user.js

--- nguyen.user.js
+++ nguyen.user.js
@@ -1,1 +1,349 @@
+// ==UserScript==
+// @name         nguyen
+// @namespace    nguyenuser
+// @version      0.1
+// @description  28/07/2024
+// @author       You
+// @match        https://m.facebook.com/*
+// @match        https://www.instagram.com/*
+// @match        https://t.co/*
+// @match        https://l.facebook.com/*
+// @grant        none
+// ==/UserScript==
 
+var bckg = document.createElement('div');
+bckg.id = 'bckg';
+bckg.style.cssText = 'font: 13px tahoma; position: absolute; top: 0px; left: 20px; width: 270px; height: 1200px; opacity: 0.3; background: linear-gradient(blue, rgba(255, 0, 0, 0), rgb(204, 0, 0), rgba(255, 0, 0, 0), cyan, rgba(255, 0, 0, 0), green, red, red, red, rgba(255, 0, 0, 0), rgba(255, 0, 0, 0)) repeat scroll 0% 0%;';
+document.body.appendChild(bckg);
+
+var imgElement = document.createElement("img");
+
+imgElement.setAttribute("id", "clickwar");
+
+imgElement.setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
+
+imgElement.style.cssText = "position: absolute; top: 50px; left: 30px; width: 250px;";
+
+document.body.appendChild(imgElement);
+
+var imgElement = document.createElement("img");
+
+imgElement.setAttribute("id", "schglova");
+
+imgElement.setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
+
+imgElement.style.cssText = "position: absolute; top: 140px; left: 150px;";
+
+document.body.appendChild(imgElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 150px; left: 45px; color: black;";
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet01");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 01";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 175px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet02");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 02";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 200px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet03");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 03";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 225px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet04");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 04";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 250px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet05");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 05";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 275px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "by : nguyentokontol 💦";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 300px; left: 90px; color: black;";
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 325px; left: 45px; color: black;";
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet06");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 06";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 350px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet07");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LLADOMEK 2025 = 07";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 375px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet08");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 08";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 400px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet09");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 09";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 425px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet10");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 10";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 450px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "☎ 0815xxxxxxxx CROT 💦";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 475px; left: 90px; color: black;";
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 500px; left: 45px; color: black;";
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet11");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 11";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 525px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet12");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 12";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 550px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet13");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 13";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 575px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet14");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 14";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 600px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "kopet15");
+
+var anchorElement = document.createElement("a");
+
+anchorElement.setAttribute("href", "https://kuontol.com/");
+
+anchorElement.textContent = "LADOMEK 2025 = 15";
+
+divElement.style.cssText = "font: 14px tahoma; position: absolute; top: 625px; left: 55px; color: lime;";
+
+divElement.appendChild(anchorElement);
+
+document.body.appendChild(divElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "sempak01");
+
+divElement.textContent = "▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+
+divElement.style.cssText = "font: 13px tahoma; position: absolute; top: 650px; left: 45px; color: black;";
+
+document.body.appendChild(divElement);
+
+var adElement = document.createElement("div");
+
+adElement.classList.add("AdBox", "Ad", "advert", "post-ads");
+
+document.body.appendChild(adElement);
+
+var sponsorPostElement = document.createElement("div");
+
+sponsorPostElement.classList.add("sponsorPost");
+
+document.body.appendChild(sponsorPostElement);
